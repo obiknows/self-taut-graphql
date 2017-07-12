76e32f64041f3dde12b28ec4eb7463a337db43b6
Sam Nnodim <sam.nnodim@gmail.com>
2017-07-12 09:33:52 -0700

    added a broken channelslist

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const ChannelsList = () =>
-     (<ul>
-       <li>Channel 1</li>
-       <li>Channel 2</li>
-     </ul>);
+// APOLLO config
+import {
+  ApolloClient, gql, graphql, ApolloProvider
+} from 'react-apollo';
+
+const client = new ApolloClient();
+
+// GQL query: get the ChannelsList
+const ChannelsListQuery = gql`
+  query ChannelsListQuery {
+    channels {
+      id
+      name
+    }
+  }
+`;
+
+const ChannelsList = ({ data: {loading, error, channels}}) => {
+  if (loading) { return <p>Loading ...</p>; }
+  if (error) { return <p>{error.message}</p>; }
+
+  return <ul>{ channels.map(ch => <li key={ch.id}>{ch.name}</li>) }</ul>;
+}
+
+const ChannelsListWithData = graphql(ChannelsListQuery)(ChannelsList);
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Apollo</h2>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to Apollo</h2>
+          </div>
+          <ChannelsListWithData />
         </div>
-        <ChannelsList />
-      </div>
+      </ApolloProvider>
     );
   }
 }
